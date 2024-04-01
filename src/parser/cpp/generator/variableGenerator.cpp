6a21098929cba94fbc6b95866533b0bdc4920d4e
1Lucas.Victor <1victor.lucassilva@gmail.com>
2024-04-01 14:41:13 -0300

    Adding a Variable Generator

---
+++ src/parser/cpp/generator/variableGenerator.cpp
@@ -0,0 +1,61 @@
+#include "variableGenerator.h"
+
+Scalar::Scalar(int initialValue, int id) { 
+    this->value = initialValue;
+    this->id = id;
+    this->name = "var" + std::to_string(id);
+}
+
+std::string Scalar::eval() {
+    return "int " + this->name + " = " + std::to_string(value) + ";";  
+}
+
+
+Array::Array(int size, int* values, int id) { 
+    this->array = values;
+    this->size = size;
+    this->id = id;
+    this->name = "var" + std::to_string(id);
+}
+
+std::string Array::eval() {
+    std:: string temp = "int* " + this->name;
+    temp = temp + " = new int[" + std::to_string(this->size) + "];";
+    return temp;
+}
+
+Array::~Array() {
+    delete [] this->array;
+}
+
+Matrix::Matrix(int rows, int columns, int* values, int id) { 
+    this->rows = rows;
+    this->cols = columns;
+    this->matrix = new int* [rows];
+    for (int i = 0; i < this->rows; i++) {
+        this->matrix[i] = new int[columns];
+    }
+    this->name = "var" + std::to_string(id);
+}
+
+std::string Matrix::eval() {
+    return "";
+}
+
+Variable* VariableFactory::createVariable(std::string type, int identifier) {
+    if(type == "scalar") {
+        return new Scalar(1, identifier);
+    }
+    if(type == "array") {
+        int size = rand() % 1000;
+        int* temp_array = new int[size];
+        for(int i = 0; i < size; i++) {
+            temp_array[i] = 0;
+        }
+        return new Array(size, temp_array, identifier);
+    }
+    // if(type == "matrix") {
+
+    // }
+    return nullptr;
+}
---
+++ src/parser/cpp/generator/variableGenerator.h
@@ -0,0 +1,49 @@
+#ifndef VARIABLEGENERATOR_H
+#define VARIABLEGENERATOR_H
+#include <string>
+#include <cstdlib> 
+#include <memory>
+
+class Variable {
+    public:
+      int id;
+      std::string name;
+      virtual ~Variable() {}
+      virtual std::string eval() = 0;
+};
+
+
+class Scalar : public Variable {
+    public:
+      int value;
+      Scalar() {}
+      Scalar(int initialValue, int id);
+      std::string eval() override;
+};
+
+class Array : public Variable {
+    public:
+      int size;
+      int* array;
+      Array(){};
+      Array(int size, int* values, int id);
+      ~Array();
+      std::string eval() override;
+};
+class Matrix : public Variable {
+    public:
+      int** matrix;
+      int rows;
+      int cols;
+      Matrix() {};
+      Matrix(int rows, int columns, int* values, int id);
+      std::string eval() override;
+};
+
+
+class VariableFactory {
+  public: 
+    static Variable* createVariable(std::string type, int identifier);
+};
+
+#endif
