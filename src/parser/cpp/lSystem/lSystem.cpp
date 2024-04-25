524ba89f18d1e33363f39df22fd0058445b3e789
Bruno Pena Baeta <brunopenabaeta@outlook.com>
2024-04-25 12:30:19 -0300

    Fixed error when parsing calls with no iteration

--- src/parser/cpp/lSystem/lSystem.cpp
+++ src/parser/cpp/lSystem/lSystem.cpp
@@ -11,67 +11,12 @@ std::vector<Token> lSystem::lSystem(int iterations, const std::vector<Production
 
     // L-System main loop
     for (int t = 0; t < iterations; t++) {
-        // Apply CALL ids to all CALL structure
-        for (auto i = outputTokens.begin(); i != outputTokens.end(); i++) {
-            int type = i->type;
-            int next2Type = std::next(i, 2)->type;
+        applyCallIds(outputTokens, callMap, callCounter);
 
-            if (type == TOK_CALL && next2Type != TOK_CALL_ID) {
-                int parens = 0;
-                std::vector<Token> callTokens = {};
-                for (auto j = std::next(i, 2); j != outputTokens.end(); j++) {
-                    if (j->type == TOK_OPAREN) {
-                        parens++;
-                    }
-                    if (j->type == TOK_CPAREN) {
-                        if (parens == 0) {
-                            break;
-                        } else {
-                            parens--;
-                        }
-                    }
-                    callTokens.push_back(*j);
-                }
-                int equalCall = findEqualCall(callMap, callTokens);
-                std::vector<Token> callIdTokens = {Token(), Token()};
-                if (equalCall == -1) {
-                    callIdTokens[0].text = std::to_string(callCounter);
-                    callMap[callTokens] = callCounter;
-                    callCounter++;
-                } else {
-                    callIdTokens[0].text = std::to_string(equalCall);
-                }
-                callIdTokens[0].type = TOK_CALL_ID;
-                callIdTokens[1].text = ",";
-                callIdTokens[1].type = TOK_COMMA;
-                std::advance(i, 2);
-                outputTokens.insert(i, std::begin(callIdTokens), std::end(callIdTokens));
-                std::advance(i, 1);
-            }
-        }
-
-        // In each loop, seek for production tokens and substitute with production rules.
-        for (auto i = outputTokens.begin(); i != outputTokens.end(); i++) {
-            if (i->type != PRODUCTION_TOK) continue;
-
-            std::string toSubstitute = i->text;
-
-            // Find equivalent rule index
-            int ruleIndex = match(toSubstitute, productionRules);
-            if (ruleIndex == -1) {
-                std::cerr << "Error: No match of " << toSubstitute << " in production rules.\n";
-                exit(1);
-            }
-
-            // Pop the token to be substituted.
-            i = outputTokens.erase(i);
-
-            // Insert rule into the vector
-            outputTokens.insert(i, std::begin(productionRules[ruleIndex].production), std::end(productionRules[ruleIndex].production));
-            // Move iterator back for it to iterate ahead again next loop.
-            i--;
-        }
+        applyProductionRules(outputTokens, productionRules);
     }
+
+    applyCallIds(outputTokens, callMap, callCounter);
 
     std::vector<Token> returnTokens{std::begin(outputTokens), std::end(outputTokens)};
 
@@ -92,4 +37,67 @@ int lSystem::findEqualCall(const std::unordered_map<std::vector<Token>, int, Tok
         return callMap.at(callTokens);
     }
     return -1;
+}
+
+void lSystem::applyCallIds(std::list<Token>& outputTokens, std::unordered_map<std::vector<Token>, int, TokenVectorHash, TokenVectorEqual>& callMap, int& callCounter) {
+    for (auto i = outputTokens.begin(); i != outputTokens.end(); i++) {
+        int type = i->type;
+        int next2Type = std::next(i, 2)->type;
+
+        if (type == TOK_CALL && next2Type != TOK_CALL_ID) {
+            int parens = 0;
+            std::vector<Token> callTokens = {};
+            for (auto j = std::next(i, 2); j != outputTokens.end(); j++) {
+                if (j->type == TOK_OPAREN) {
+                    parens++;
+                }
+                if (j->type == TOK_CPAREN) {
+                    if (parens == 0) {
+                        break;
+                    } else {
+                        parens--;
+                    }
+                }
+                callTokens.push_back(*j);
+            }
+            int equalCall = findEqualCall(callMap, callTokens);
+            std::vector<Token> callIdTokens = {Token(), Token()};
+            if (equalCall == -1) {
+                callIdTokens[0].text = std::to_string(callCounter);
+                callMap[callTokens] = callCounter;
+                callCounter++;
+            } else {
+                callIdTokens[0].text = std::to_string(equalCall);
+            }
+            callIdTokens[0].type = TOK_CALL_ID;
+            callIdTokens[1].text = ",";
+            callIdTokens[1].type = TOK_COMMA;
+            std::advance(i, 2);
+            outputTokens.insert(i, std::begin(callIdTokens), std::end(callIdTokens));
+            std::advance(i, 1);
+        }
+    }
+}
+
+void lSystem::applyProductionRules(std::list<Token>& outputTokens, const std::vector<ProductionRule>& productionRules) {
+    for (auto i = outputTokens.begin(); i != outputTokens.end(); i++) {
+        if (i->type != PRODUCTION_TOK) continue;
+
+        std::string toSubstitute = i->text;
+
+        // Find equivalent rule index
+        int ruleIndex = match(toSubstitute, productionRules);
+        if (ruleIndex == -1) {
+            std::cerr << "Error: No match of " << toSubstitute << " in production rules.\n";
+            exit(1);
+        }
+
+        // Pop the token to be substituted.
+        i = outputTokens.erase(i);
+
+        // Insert rule into the vector
+        outputTokens.insert(i, std::begin(productionRules[ruleIndex].production), std::end(productionRules[ruleIndex].production));
+        // Move iterator back for it to iterate ahead again next loop.
+        i--;
+    }
 }
--- src/parser/cpp/lSystem/lSystem.h
+++ src/parser/cpp/lSystem/lSystem.h
@@ -14,6 +14,8 @@ namespace lSystem {
 std::vector<Token> lSystem(int, const std::vector<ProductionRule>&, const std::vector<Token>&);
 int match(std::string, const std::vector<ProductionRule>&);
 int findEqualCall(const std::unordered_map<std::vector<Token>, int, TokenVectorHash, TokenVectorEqual>&, const std::vector<Token>&);
+void applyCallIds(std::list<Token>&, std::unordered_map<std::vector<Token>, int, TokenVectorHash, TokenVectorEqual>&, int&);
+void applyProductionRules(std::list<Token>&, const std::vector<ProductionRule>&);
 }  // namespace lSystem
 
 #endif
