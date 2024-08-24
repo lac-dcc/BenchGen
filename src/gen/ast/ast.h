8fbcf50996d99d632ac055fdcf8966e476b3c7c6
Bruno Pena Baeta <brunopenabaeta@outlook.com>
2024-08-23 22:18:38 -0300

    Added documentation to parser and ast

--- src/gen/ast/ast.h
+++ src/gen/ast/ast.h
@@ -7,21 +7,59 @@
 #include "../shared/enums.h"
 #include "../shared/globalStructs.h"
 
-void printIndentationSpaces(int);
+/**
+ * @brief Prints a specified number of indentation spaces.
+ *
+ * Used for formatting output to visualize the structure of the AST.
+ *
+ * @param indent The number of spaces to print.
+ */
+void printIndentationSpaces(int indent);
 
+/**
+ * @brief Generates a condition string for an if statement.
+ *
+ * Generates a condition based on the generator's current context, either returning
+ * a random condition or a condition based on PATH variables.
+ *
+ * @param generator The generator object used to track the current state of code generation.
+ * @return A string representing the generated condition.
+ */
 std::string generateIfCondition(Generator& generator);
 
+/**
+ * @brief Base class for all nodes in the abstract syntax tree (AST).
+ *
+ * Defines the interface for generating code and printing the AST structure.
+ */
 class Node {
    public:
     virtual ~Node() = default;
+
+    /**
+     * @brief Generates code for this AST node.
+     *
+     * @param generator The generator object used to manage code generation.
+     */
     virtual void gen(Generator&) = 0;
+
+    /**
+     * @brief Prints the structure of this AST node.
+     *
+     * @param indent The number of spaces to indent the output.
+     */
     virtual void print(int indent = 0) = 0;
 };
 
+/**
+ * @brief Represents a block of statements in the AST.
+ *
+ * Contains a statement and the following code block.
+ */
 class StatementCode : public Node {
    private:
-    std::shared_ptr<Node> stmt;
-    std::shared_ptr<Node> code;
+    std::shared_ptr<Node> stmt;  // The statement node
+    std::shared_ptr<Node> code;  // The following code block
 
    public:
     StatementCode(std::shared_ptr<Node> stmt, std::shared_ptr<Node> code) : stmt(stmt), code(code) {
@@ -32,16 +70,26 @@ class StatementCode : public Node {
     void print(int) override;
 };
 
+/**
+ * @brief Represents a lambda expression in the AST.
+ *
+ * Currently, this class does not generate any code.
+ */
 class LambdaCode : public Node {
    public:
     void gen(Generator&) override;
 
-    void print(int ident) override;
+    void print(int indent) override;
 };
 
+/**
+ * @brief Represents an identifier in the AST.
+ *
+ * Holds the name of the identifier.
+ */
 class Id : public Node {
    private:
-    std::string id;
+    std::string id;  // The identifier's name
 
    public:
     Id(std::string id) : id(id) {
@@ -49,40 +97,65 @@ class Id : public Node {
 
     void gen(Generator&) override;
 
-    void print(int ident) override;
+    void print(int indent) override;
 };
 
+/**
+ * @brief Represents an insert operation in the AST.
+ *
+ * Generates code to insert into a variable.
+ */
 class Insert : public Node {
    public:
     void gen(Generator&) override;
 
-    void print(int ident) override;
+    void print(int indent) override;
 };
 
+/**
+ * @brief Represents a remove operation in the AST.
+ *
+ * Generates code to remove from a variable.
+ */
 class Remove : public Node {
    public:
     void gen(Generator&) override;
 
-    void print(int ident) override;
+    void print(int indent) override;
 };
 
+/**
+ * @brief Represents a new variable creation in the AST.
+ *
+ * Generates code to create a new variable.
+ */
 class New : public Node {
    public:
     void gen(Generator&) override;
 
-    void print(int ident) override;
+    void print(int indent) override;
 };
 
+/**
+ * @brief Represents a contains operation in the AST.
+ *
+ * Generates code to check if a variable contains a value.
+ */
 class Contains : public Node {
    public:
     void gen(Generator&) override;
 
-    void print(int ident) override;
+    void print(int indent) override;
 };
 
+/**
+ * @brief Represents a loop in the AST.
+ *
+ * Contains the code block to be executed in the loop.
+ */
 class Loop : public Node {
    private:
-    std::shared_ptr<Node> code;
+    std::shared_ptr<Node> code;  // The code block to be executed in the loop
 
    public:
     Loop(std::shared_ptr<Node> code) : code(code) {
@@ -90,16 +163,21 @@ class Loop : public Node {
 
     void gen(Generator&) override;
 
-    void print(int ident) override;
+    void print(int indent) override;
 };
 
+/**
+ * @brief Represents a function call in the AST.
+ *
+ * Manages the function's parameters, ID, and code block.
+ */
 class Call : public Node {
    private:
-    int id;
-    std::shared_ptr<Node> code;
+    int id;                      // The ID of the function being called
+    std::shared_ptr<Node> code;  // The code block of the function
 
    public:
-    int conditionalCounts;
+    int conditionalCounts;  // Tracks the number of conditional statements in the call
 
     Call(int id, std::shared_ptr<Node> code) : id(id), code(code), conditionalCounts(0) {
     }
@@ -107,22 +185,37 @@ class Call : public Node {
     Call() : conditionalCounts(0) {
     }
 
+    /**
+     * @brief Sets the ID of the function being called.
+     *
+     * @param id The function ID.
+     */
     void setId(int id) {
         this->id = id;
     }
 
+    /**
+     * @brief Sets the code block of the function being called.
+     *
+     * @param code The code block.
+     */
     void setCode(std::shared_ptr<Node> code) {
         this->code = code;
     }
 
     void gen(Generator&) override;
 
-    void print(int ident) override;
+    void print(int indent) override;
 };
 
+/**
+ * @brief Represents a sequence of operations in the AST.
+ *
+ * Contains the code block for the sequence.
+ */
 class Seq : public Node {
    private:
-    std::shared_ptr<Node> code;
+    std::shared_ptr<Node> code;  // The code block for the sequence
 
    public:
     Seq(std::shared_ptr<Node> code) : code(code) {
@@ -130,12 +223,17 @@ class Seq : public Node {
 
     void gen(Generator&) override;
 
-    void print(int ident) override;
+    void print(int indent) override;
 };
 
+/**
+ * @brief Represents an if statement in the AST.
+ *
+ * Contains the parameters and the else clause for the if statement.
+ */
 class If : public Node {
    private:
-    std::shared_ptr<Node> ifParam;
+    std::shared_ptr<Node> ifParam;  // The parameters for the if statement
 
    public:
     If(std::shared_ptr<Node> ifParam) : ifParam(ifParam) {
@@ -143,13 +241,18 @@ class If : public Node {
 
     void gen(Generator&) override;
 
-    void print(int ident) override;
+    void print(int indent) override;
 };
 
+/**
+ * @brief Represents the parameters of an if statement in the AST.
+ *
+ * Contains the code block for the if statement and the else clause.
+ */
 class IfParam : public Node {
    private:
-    std::shared_ptr<Node> code;
-    std::shared_ptr<Node> else_;
+    std::shared_ptr<Node> code;   // The code block for the if statement
+    std::shared_ptr<Node> else_;  // The else clause
 
    public:
     IfParam(std::shared_ptr<Node> code, std::shared_ptr<Node> else_) : code(code), else_(else_) {
@@ -157,12 +260,17 @@ class IfParam : public Node {
 
     void gen(Generator&) override;
 
-    void print(int ident) override;
+    void print(int indent) override;
 };
 
+/**
+ * @brief Represents an if statement with no parameters in the AST.
+ *
+ * Contains the else clause for the if statement.
+ */
 class NoParamIf : public Node {
    private:
-    std::shared_ptr<Node> else_;
+    std::shared_ptr<Node> else_;  // The else clause
 
    public:
     NoParamIf(std::shared_ptr<Node> else_) : else_(else_) {
@@ -170,12 +278,17 @@ class NoParamIf : public Node {
 
     void gen(Generator&) override;
 
-    void print(int ident) override;
+    void print(int indent) override;
 };
 
+/**
+ * @brief Represents an else clause with a code block in the AST.
+ *
+ * Contains the code block for the else clause.
+ */
 class CodeElse : public Node {
    private:
-    std::shared_ptr<Node> code;
+    std::shared_ptr<Node> code;  // The code block for the else clause
 
    public:
     CodeElse(std::shared_ptr<Node> code) : code(code) {
@@ -183,14 +296,19 @@ class CodeElse : public Node {
 
     void gen(Generator&) override;
 
-    void print(int ident) override;
+    void print(int indent) override;
 };
 
+/**
+ * @brief Represents an else clause with no code block in the AST.
+ *
+ * This class does not generate any code.
+ */
 class NoCodeElse : public Node {
    public:
     void gen(Generator&) override;
 
-    void print(int ident) override;
+    void print(int indent) override;
 };
 
 #endif
--- src/gen/parser/parser.cpp
+++ src/gen/parser/parser.cpp
@@ -1,5 +1,13 @@
 #include "parser.h"
 
+/**
+ * @brief Matches the current token against the expected symbol and advances to the next token.
+ *
+ * If the current token matches the expected symbol, it moves to the next token.
+ * Otherwise, it prints an error message indicating a parsing error.
+ *
+ * @param symbol The expected token type to match.
+ */
 void Parser::match(int symbol) {
     if (tokens[tokenIndex].type == symbol) {
         tokenIndex++;
@@ -9,9 +17,17 @@ void Parser::match(int symbol) {
     }
 }
 
+/**
+ * @brief Parses a sequence of statements into a code block.
+ *
+ * This function handles parsing different types of statements, using recursive calls
+ * to build a tree of nodes representing the structure of the code.
+ *
+ * @return A shared pointer to the root node of the parsed code block.
+ */
 std::shared_ptr<Node> Parser::parse_CODE() {
     if ((int)tokens.size() <= tokenIndex) {
-        return std::make_shared<LambdaCode>(LambdaCode());
+        return std::make_shared<LambdaCode>(LambdaCode()); // Return an empty code block if no more tokens
     }
     switch (tokens[tokenIndex].type) {
         case TOK_INSERT:
@@ -23,10 +39,10 @@ std::shared_ptr<Node> Parser::parse_CODE() {
         case TOK_SEQ:
         case TOK_IF:
         case TOK_ID:
-            return std::make_shared<StatementCode>(StatementCode(parse_STATEMENT(), parse_CODE()));
+            return std::make_shared<StatementCode>(StatementCode(parse_STATEMENT(), parse_CODE())); // Parse statement followed by code
         case TOK_CPAREN:
         case TOK_COMMA:
-            return std::make_shared<LambdaCode>(LambdaCode());
+            return std::make_shared<LambdaCode>(LambdaCode()); // Return an empty code block
         default:
             // TODO: Error handling
             std::cout << "ERROR PARSING CODE! AT " << tokens[tokenIndex].type << std::endl;
@@ -35,6 +51,14 @@ std::shared_ptr<Node> Parser::parse_CODE() {
     return nullptr;
 }
 
+/**
+ * @brief Parses a single statement.
+ *
+ * This function identifies the type of statement and parses it accordingly,
+ * creating the appropriate node type for each statement.
+ *
+ * @return A shared pointer to the node representing the parsed statement.
+ */
 std::shared_ptr<Node> Parser::parse_STATEMENT() {
     switch (tokens[tokenIndex].type) {
         case TOK_INSERT:
@@ -60,7 +84,7 @@ std::shared_ptr<Node> Parser::parse_STATEMENT() {
             match(tokens[tokenIndex].type);
             match(TOK_OPAREN);
             match(TOK_CALL_ID);
-            int callId = std::stoi(tokens[tokenIndex - 1].text);
+            int callId = std::stoi(tokens[tokenIndex - 1].text); // Get the call ID
             match(TOK_COMMA);
             Call call = Call();
             currentCall.push(&call);
@@ -87,7 +111,7 @@ std::shared_ptr<Node> Parser::parse_STATEMENT() {
             return std::make_shared<If>(If(parse_IFPARAM()));
         case TOK_ID:
             match(TOK_ID);
-            return std::make_shared<Id>(Id(tokens[tokenIndex - 1].text));
+            return std::make_shared<Id>(Id(tokens[tokenIndex - 1].text)); // Create an ID node with the text of the matched token
         default:
             // TODO: Error handling
             std::cout << "ERROR PARSING INSTRUCTION! AT " << tokens[tokenIndex].type << std::endl;
@@ -96,6 +120,14 @@ std::shared_ptr<Node> Parser::parse_STATEMENT() {
     return nullptr;
 }
 
+/**
+ * @brief Parses the parameters for an if statement.
+ *
+ * This function parses the parameters following an if statement, which can be either
+ * a block of code or an underscore followed by a comma, indicating a no-parameter if statement.
+ *
+ * @return A shared pointer to the node representing the parsed if parameters.
+ */
 std::shared_ptr<Node> Parser::parse_IFPARAM() {
     switch (tokens[tokenIndex].type) {
         case TOK_IF:
@@ -107,15 +139,15 @@ std::shared_ptr<Node> Parser::parse_IFPARAM() {
         case TOK_NEW:
         case TOK_CONTAINS:
         case TOK_ID: {
-            std::shared_ptr<Node> code = parse_CODE();
+            std::shared_ptr<Node> code = parse_CODE(); // Parse the code block
             match(TOK_COMMA);
-            return std::make_shared<IfParam>(IfParam(code, parse_ELSE()));
+            return std::make_shared<IfParam>(IfParam(code, parse_ELSE())); // Create an IfParam node with the parsed code and else block
         }
         case TOK_UNDERLINE:
             match(TOK_UNDERLINE);
         case TOK_COMMA:
             match(TOK_COMMA);
-            return std::make_shared<NoParamIf>(NoParamIf(parse_ELSE()));
+            return std::make_shared<NoParamIf>(NoParamIf(parse_ELSE())); // Create a NoParamIf node
         default:
             // TODO: Error handling
             std::cout << "ERROR PARSING IFPARAM! AT " << tokens[tokenIndex].type << std::endl;
@@ -124,6 +156,14 @@ std::shared_ptr<Node> Parser::parse_IFPARAM() {
     return nullptr;
 }
 
+/**
+ * @brief Parses the else clause of an if statement.
+ *
+ * This function handles parsing the else clause, which can either contain a block of code
+ * or be empty, depending on the syntax following the if statement.
+ *
+ * @return A shared pointer to the node representing the parsed else clause.
+ */
 std::shared_ptr<Node> Parser::parse_ELSE() {
     switch (tokens[tokenIndex].type) {
         case TOK_IF:
@@ -135,15 +175,15 @@ std::shared_ptr<Node> Parser::parse_ELSE() {
         case TOK_NEW:
         case TOK_CONTAINS:
         case TOK_ID: {
-            std::shared_ptr<Node> code = parse_CODE();
+            std::shared_ptr<Node> code = parse_CODE(); // Parse the code block
             match(TOK_CPAREN);
-            return std::make_shared<CodeElse>(CodeElse(code));
+            return std::make_shared<CodeElse>(CodeElse(code)); // Create a CodeElse node
         }
         case TOK_UNDERLINE:
             match(TOK_UNDERLINE);
         case TOK_CPAREN:
             match(TOK_CPAREN);
-            return std::make_shared<NoCodeElse>(NoCodeElse());
+            return std::make_shared<NoCodeElse>(NoCodeElse()); // Create a NoCodeElse node
         default:
             // TODO: Error handling
             std::cout << "ERROR PARSING ELSE! AT " << tokens[tokenIndex].type << std::endl;
@@ -152,14 +192,35 @@ std::shared_ptr<Node> Parser::parse_ELSE() {
     return nullptr;
 }
 
+/**
+ * @brief Sets the tokens for the parser to process.
+ *
+ * This function initializes the parser's token list with the provided tokens.
+ *
+ * @param _tokens The vector of tokens to be parsed.
+ */
 void Parser::setTokens(std::vector<Token> _tokens) {
     tokens = _tokens;
 }
 
+/**
+ * @brief Initiates the parsing process and builds the abstract syntax tree (AST).
+ *
+ * This function starts the parsing process by calling the parse_CODE function,
+ * which builds the AST from the provided tokens.
+ */
 void Parser::parse() {
     AST = parse_CODE();
 }
 
+/**
+ * @brief Returns the root of the abstract syntax tree (AST).
+ *
+ * This function provides access to the root node of the AST, representing the
+ * parsed structure of the input code.
+ *
+ * @return A shared pointer to the root node of the AST.
+ */
 std::shared_ptr<Node> Parser::getAST() {
     return AST;
 }
