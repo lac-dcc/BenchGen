4a78d7e41efb978c83be888e6dd81adb339cfd5c
Bruno Pena Baeta <brunopenabaeta@outlook.com>
2024-09-05 17:32:19 -0300

    Fixed memory issue and removed documentation from .cpp files

--- src/gen/generator/generatorVariable.cpp
+++ src/gen/generator/generatorVariable.cpp
@@ -1,15 +1,5 @@
 #include "generatorVariable.h"
 
-/**
- * @brief Factory method to create a GeneratorVariable of a specified type.
- *
- * This method creates a new variable of the specified type and initializes it with
- * default values. The created variable is returned as a pointer to a GeneratorVariable.
- *
- * @param type The type of variable to create (e.g., "array").
- * @param identifier A unique identifier for the variable.
- * @return A pointer to the created GeneratorVariable object.
- */
 GeneratorVariable* VariableFactory::createVariable(std::string type, int identifier) {
     if (type == VarTypes::ARRAY) {
         int size = rand() % 1000;  // Random size for the array
@@ -19,14 +9,6 @@ GeneratorVariable* VariableFactory::createVariable(std::string type, int identif
     return nullptr;
 }
 
-/**
- * @brief Generates a string representation of the variable type.
- *
- * This method creates a temporary variable of the specified type to retrieve its type string.
- *
- * @param type The type of variable to create (e.g., "array").
- * @return A string representing the type of the variable.
- */
 std::string VariableFactory::genTypeString(std::string type) {
     GeneratorVariable* var = createVariable(type, 0);
     std::string typeString = var->typeString;
@@ -34,15 +16,6 @@ std::string VariableFactory::genTypeString(std::string type) {
     return typeString;
 }
 
-/**
- * @brief Generates include statements required for the specified variable type.
- *
- * This method creates a temporary variable of the specified type to determine the necessary include
- * statements for that type.
- *
- * @param type The type of variable to create (e.g., "array").
- * @return A vector of strings representing the include directives.
- */
 std::vector<std::string> VariableFactory::genIncludes(std::string type) {
     GeneratorVariable* var = createVariable(type, 0);
     std::vector<std::string> includes = var->genIncludes();
@@ -50,14 +23,6 @@ std::vector<std::string> VariableFactory::genIncludes(std::string type) {
     return includes;
 }
 
-/**
- * @brief Generates global variable declarations for the specified variable type.
- *
- * This method creates a temporary variable of the specified type to retrieve its global variable declarations.
- *
- * @param type The type of variable to create (e.g., "array").
- * @return A vector of strings representing global variable declarations.
- */
 std::vector<std::string> VariableFactory::genGlobalVars(std::string type) {
     GeneratorVariable* var = createVariable(type, 0);
     std::vector<std::string> globalVars = var->genGlobalVars();
@@ -65,17 +30,6 @@ std::vector<std::string> VariableFactory::genGlobalVars(std::string type) {
     return globalVars;
 }
 
-/**
- * @brief Generates parameter handling code for functions using the specified variable type.
- *
- * This method creates a temporary variable of the specified type to generate code for handling
- * parameters in functions.
- *
- * @param type The type of variable to create (e.g., "array").
- * @param paramName The name of the parameter.
- * @param varsParams List of variables to be used as parameters.
- * @return A vector of strings representing the code for parameter handling.
- */
 std::vector<std::string> VariableFactory::genParams(std::string type, std::string paramName, std::vector<GeneratorVariable*> varsParams) {
     GeneratorVariable* var = createVariable(type, 0);
     std::vector<std::string> params = var->genParams(paramName, varsParams);
@@ -85,14 +39,6 @@ std::vector<std::string> VariableFactory::genParams(std::string type, std::strin
 
 // ARRAY
 
-/**
- * @brief Constructor for the Array class.
- *
- * Initializes an Array object with a specified size, values, and identifier.
- *
- * @param size The size of the array.
- * @param id The unique identifier for the array.
- */
 Array::Array(int size, int id) {
     this->typeString = "Array";
     this->totalSize = size;
@@ -100,131 +46,74 @@ Array::Array(int size, int id) {
     this->name = VarTypes::ARRAY + std::to_string(id);
 }
 
-/**
- * @brief Generates code to create a new array variable.
- *
- * This method returns a vector of strings, each representing a line of code to
- * initialize a new array variable. If the array is created within a function,
- * it includes additional logic to handle memory allocation.
- *
- * @param inFunction A boolean indicating whether the array is being created inside a function.
- * @return A vector of strings, each representing a line of code.
- */
 std::vector<std::string> Array::new_(bool inFunction) {
-    std::vector<std::string> temp = {this->typeString + " " + this->name + ";"};
+    std::vector<std::string> temp = {this->typeString + "* " + this->name + ";"};
     if (inFunction) {
         temp.push_back("if (pCounter > 0) {");
         temp.push_back("   " + this->name + " = vars->data[--pCounter];");
-        temp.push_back("   " + this->name + ".refC++;");
+        temp.push_back("   " + this->name + "->refC++;");
         temp.push_back("} else {");
-        temp.push_back("   " + this->name + ".size = " + std::to_string(this->totalSize) + ";");
-        temp.push_back("   " + this->name + ".refC = 1;");
-        temp.push_back("   " + this->name + ".data = (unsigned int*)malloc(" + this->name + ".size*sizeof(unsigned int));");
-        temp.push_back("   memset(" + this->name + ".data, 0, " + this->name + ".size*sizeof(unsigned int));");
+        temp.push_back("   " + this->name + " = (" + this->typeString + "*)malloc(sizeof(" + this->typeString + "));");
+        temp.push_back("   " + this->name + "->size = " + std::to_string(this->totalSize) + ";");
+        temp.push_back("   " + this->name + "->refC = 1;");
+        temp.push_back("   " + this->name + "->data = (unsigned int*)malloc(" + this->name + "->size*sizeof(unsigned int));");
+        temp.push_back("   memset(" + this->name + "->data, 0, " + this->name + "->size*sizeof(unsigned int));");
         temp.push_back("}");
     } else {
-        temp.push_back(this->name + ".size = " + std::to_string(this->totalSize) + ";");
-        temp.push_back(this->name + ".refC = 1;");
-        temp.push_back(this->name + ".data = (unsigned int*)malloc(" + this->name + ".size*sizeof(unsigned int));");
-        temp.push_back("memset(" + this->name + ".data, 0, " + this->name + ".size*sizeof(unsigned int));");
+        temp.push_back(this->name + " = (" + this->typeString + "*)malloc(sizeof(" + this->typeString + "));");
+        temp.push_back(this->name + "->size = " + std::to_string(this->totalSize) + ";");
+        temp.push_back(this->name + "->refC = 1;");
+        temp.push_back(this->name + "->data = (unsigned int*)malloc(" + this->name + "->size*sizeof(unsigned int));");
+        temp.push_back("memset(" + this->name + "->data, 0, " + this->name + "->size*sizeof(unsigned int));");
     }
     return temp;
 }
 
-/**
- * @brief Generates code to insert elements into the array.
- *
- * This method returns a vector of strings representing the lines of code
- * required to increment each element of the array.
- *
- * @return A vector of strings, each representing a line of code.
- */
 std::vector<std::string> Array::insert() {
-    std::vector<std::string> temp = {"for (int i = 0; i < " + this->name + ".size; i++) {"};
-    temp.push_back("   " + this->name + ".data[i]++;");
+    std::vector<std::string> temp = {"for (int i = 0; i < " + this->name + "->size; i++) {"};
+    temp.push_back("   " + this->name + "->data[i]++;");
     temp.push_back("}");
     return temp;
 }
 
-/**
- * @brief Generates code to remove elements from the array.
- *
- * This method returns a vector of strings representing the lines of code
- * required to decrement each element of the array.
- *
- * @return A vector of strings, each representing a line of code.
- */
 std::vector<std::string> Array::remove() {
-    std::vector<std::string> temp = {"for (int i = 0; i < " + this->name + ".size; i++) {"};
-    temp.push_back("   " + this->name + ".data[i]--;");
+    std::vector<std::string> temp = {"for (int i = 0; i < " + this->name + "->size; i++) {"};
+    temp.push_back("   " + this->name + "->data[i]--;");
     temp.push_back("}");
     return temp;
 }
 
-/**
- * @brief Generates code to check if the array contains a specific value.
- *
- * This method returns a vector of strings representing the lines of code
- * required to search for a specific value in the array. It can also handle
- * the return of the array if specified.
- *
- * @param shouldReturn A boolean indicating whether to return the array if the value is found.
- * @return A vector of strings, each representing a line of code.
- */
 std::vector<std::string> Array::contains(bool shouldReturn) {
     int compare = rand() % 100;  // Random value to compare against
     std::vector<std::string> temp = {};
-    temp.push_back("for (int i = 0; i < " + this->name + ".size; i++) {");
-    temp.push_back("   if (" + this->name + ".data[i] == " + std::to_string(compare) + ") { ");
+    temp.push_back("for (int i = 0; i < " + this->name + "->size; i++) {");
+    temp.push_back("   if (" + this->name + "->data[i] == " + std::to_string(compare) + ") { ");
     if (shouldReturn) {
         temp.push_back("      return " + this->name + ";");
     } else {
-        temp.push_back("      " + this->name + ".data[i] += " + std::to_string(compare) + ";");
+        temp.push_back("      " + this->name + "->data[i] += " + std::to_string(compare) + ";");
     }
     temp.push_back("   }");
     temp.push_back("}");
     return temp;
 }
 
-/**
- * @brief Generates code to free the memory allocated for the array.
- *
- * This method returns a vector of strings representing the lines of code
- * required to free the memory if the reference count drops to zero.
- *
- * @return A vector of strings, each representing a line of code.
- */
 std::vector<std::string> Array::free() {
     std::vector<std::string> temp = {};
-    temp.push_back(this->name + ".refC--;");
-    temp.push_back("if(" + this->name + ".refC == 0) {");
-    temp.push_back("   free(" + this->name + ".data);");
+    temp.push_back(this->name + "->refC--;");
+    temp.push_back("if(" + this->name + "->refC == 0) {");
+    temp.push_back("   free(" + this->name + "->data);");
+    temp.push_back("   free(" + this->name + ");");
     temp.push_back("}");
     return temp;
 }
 
-/**
- * @brief Generates the necessary include statements for array operations.
- *
- * This method returns a vector of strings containing the required include directives
- * for working with arrays.
- *
- * @return A vector of strings, each representing an include directive.
- */
 std::vector<std::string> Array::genIncludes() {
     std::vector<std::string> temp = {};
     temp.push_back("#include <string.h>");
     return temp;
 }
 
-/**
- * @brief Generates global variable declarations for array structures.
- *
- * This method returns a vector of strings representing the typedefs and
- * struct definitions necessary for array operations.
- *
- * @return A vector of strings, each representing a global variable or type definition.
- */
 std::vector<std::string> Array::genGlobalVars() {
     std::vector<std::string> temp = {};
     temp.push_back("typedef struct {");
@@ -233,37 +122,22 @@ std::vector<std::string> Array::genGlobalVars() {
     temp.push_back("   size_t refC;");
     temp.push_back("} " + this->typeString + ";");
     temp.push_back("typedef struct {");
-    temp.push_back("   Array* data;");
+    temp.push_back("   Array** data;");
     temp.push_back("   size_t size;");
     temp.push_back("} " + this->typeString + "Param;");
     return temp;
 }
 
-/**
- * @brief Generates parameter handling code for functions using arrays.
- *
- * This method returns a vector of strings representing the lines of code
- * required to initialize and handle parameters of type array in functions.
- *
- * @param paramName The name to use for the parameter.
- * @param varsParams The list of variables to be passed as parameters.
- * @return A vector of strings, each representing a line of code for parameter handling.
- */
 std::vector<std::string> Array::genParams(std::string paramName, std::vector<GeneratorVariable*> varsParams) {
     std::vector<std::string> temp = {};
     temp.push_back(this->typeString + "Param " + paramName + ";");
     temp.push_back(paramName + ".size = " + std::to_string(varsParams.size()) + ";");
-    temp.push_back(paramName + ".data = (" + this->typeString + "*)malloc(" + paramName + ".size*sizeof(" + this->typeString + "));");
+    temp.push_back(paramName + ".data = (" + this->typeString + "**)malloc(" + paramName + ".size*sizeof(" + this->typeString + "*));");
     for (int i = 0; i < varsParams.size(); i++) {
         temp.push_back(paramName + ".data[" + std::to_string(i) + "] = " + varsParams[i]->name + ";");
     }
     return temp;
 }
 
-/**
- * @brief Destructor for the Array class.
- *
- * This destructor currently does not perform any specific actions.
- */
 Array::~Array() {
 }
--- src/gen/lexer/lexer.cpp
+++ src/gen/lexer/lexer.cpp
@@ -3,23 +3,12 @@
 using std::ifstream;
 using std::regex;
 
-/**
- * @brief Constructs a Lexer object and loads the configuration rules.
- */
 Lexer::Lexer() {
     loadConfiguration();
 }
 
-/**
- * @brief Reads the content of a specified file into a vector of strings.
- *
- * Each line of the file is stored as a separate string in the vector.
- *
- * @param fileName The path to the file to be read.
- * @return A vector of strings, each representing a line from the file.
- */
 std::vector<std::string> Lexer::readFile(std::string fileName) {
-    std::vector<std::string> code = {};  // Vector to store file lines
+    std::vector<std::string> code = {};
     ifstream file("./" + fileName);
     if (file.is_open()) {
         std::string line;
@@ -33,20 +22,10 @@ std::vector<std::string> Lexer::readFile(std::string fileName) {
     return code;
 }
 
-/**
- * @brief Tokenizes a given string based on the loaded lexer rules.
- *
- * This function processes the input string to extract tokens according to the
- * defined lexer rules, handling spaces and matching the longest possible lexeme.
- *
- * @param code The string to tokenize.
- * @return A vector of Tokens extracted from the input string.
- */
 std::vector<Token> Lexer::tokenize(std::string code) {
-    std::vector<Token> tokens = {};  // Vector to store tokens
+    std::vector<Token> tokens = {};
     for (int i = 0; i < code.size(); i++) {
-        std::string lexeme = code.substr(i, 1);  // Building lexeme
-
+        std::string lexeme = code.substr(i, 1);
         if (lexeme[0] == ' ') {
             continue;
         }
@@ -82,15 +61,6 @@ std::vector<Token> Lexer::tokenize(std::string code) {
     return tokens;
 }
 
-/**
- * @brief Checks if a string matches any of the lexer rules.
- *
- * This function uses regular expressions to match the input string against
- * each of the defined lexer rules.
- *
- * @param s The string to match against the lexer rules.
- * @return True if the input matches any rule, otherwise false.
- */
 bool Lexer::matchAnyRule(std::string s) {
     for (LexerRule rule : rules) {
         regex re(rule.regex);
@@ -101,15 +71,6 @@ bool Lexer::matchAnyRule(std::string s) {
     return false;
 }
 
-/**
- * @brief Matches a string to a specific token type based on lexer rules.
- *
- * This function returns the token type of the first rule that matches the
- * given lexeme using regular expressions.
- *
- * @param lexeme The string to match.
- * @return The type of the token matched, or an error type if no match is found.
- */
 int Lexer::matchToken(std::string lexeme) {
     for (LexerRule rule : rules) {
         regex re(rule.regex);
@@ -121,14 +82,7 @@ int Lexer::matchToken(std::string lexeme) {
     return -1;
 }
 
-/**
- * @brief Loads lexer configuration rules.
- *
- * This function initializes the lexer rules with predefined types and regular
- * expressions that will be used for tokenizing input.
- */
 void Lexer::loadConfiguration() {
-    // Defining lexer rules for different token types
     LexerRule lrIf;
     LexerRule lrLoop;
     LexerRule lrCall;
@@ -188,18 +142,9 @@ void Lexer::loadConfiguration() {
         lrOParen, lrCParen, lrComma, lrEqual, lrUnderline, lrComment, lrId, lrError};
 }
 
-/**
- * @brief Gets the tokens from a given file.
- *
- * This function reads the content of a file and tokenizes each line
- * to generate a vector of tokens.
- *
- * @param fileName The path to the file from which tokens are to be generated.
- * @return A vector of Tokens extracted from the file.
- */
 std::vector<Token> Lexer::getTokens(std::string fileName) {
-    std::vector<std::string> fileLines = readFile(fileName);  // Reading file lines
-    std::vector<Token> tokens = {};                           // Vector to store tokens
+    std::vector<std::string> fileLines = readFile(fileName);
+    std::vector<Token> tokens = {};
     for (auto line : fileLines) {
         std::vector<Token> line_tokens = tokenize(line);
         tokens.insert(tokens.end(), line_tokens.begin(), line_tokens.end());
@@ -207,17 +152,8 @@ std::vector<Token> Lexer::getTokens(std::string fileName) {
     return tokens;
 }
 
-/**
- * @brief Extracts production rules from a given file.
- *
- * This function reads a file, tokenizes its content, and extracts production
- * rules defined in the file.
- *
- * @param fileName The path to the file containing production rules.
- * @return A vector of ProductionRules extracted from the file.
- */
 std::vector<ProductionRule> Lexer::getProductionRules(std::string fileName) {
-    std::vector<ProductionRule> productionRules = {};  // Vector to store production rules
+    std::vector<ProductionRule> productionRules = {};
     std::vector<Token> tokens = getTokens(fileName);
     for (int i = 0; i < tokens.size(); i++) {
         if (tokens[i].type == TOK_ID) {
