63d4417844a314d8de22543596fca3f8975a31a5
Bruno Pena Baeta <brunopenabaeta@outlook.com>
2024-11-23 01:21:59 -0300

    Added "Programs that does not grow" to the grammar enumeration program analysis

--- src/scripts/grammarEnumeration/programsAnalysis.py
+++ src/scripts/grammarEnumeration/programsAnalysis.py
@@ -25,37 +25,59 @@ def createProgramFiles(obj_folder, current_program):
     with open(os.path.join(obj_folder, "pr.txt"), "w") as pr_file:
         pr_file.write(f"A = {program.rule_a};\nB = {program.rule_b};")
 
-def generateProgram():
+def generateProgram(iterations):
     gen_folder = './../../gen'
     obj_folder_relative = './../scripts/grammarEnumeration/obj'
-    subprocess.run(f"cd {gen_folder} && rm -r program", shell=True)
-    subprocess.run(f"cd {gen_folder} && ./benchGen 5 {obj_folder_relative}/pr.txt {obj_folder_relative}/ss.txt program array", shell=True, stdout=subprocess.DEVNULL, check=True)
-    return f"{gen_folder}/program"
+    folder_name = f"program{iterations}"
+    subprocess.run(f"cd {gen_folder} && rm -r {folder_name}", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.run(f"cd {gen_folder} && ./benchGen {iterations} {obj_folder_relative}/pr.txt {obj_folder_relative}/ss.txt {folder_name} array", shell=True, stdout=subprocess.DEVNULL, check=True)
+    return f"{gen_folder}/{folder_name}"
 
-def compileProgram(obj_folder, program_folder_path):
-    stderr_temp = os.path.join(obj_folder, "stderr_temp.txt")
+def compileProgram(obj_folder, program_folder_path, number):
+    stderr_temp = os.path.join(obj_folder, f"stderr_temp{number}.txt")
+    subprocess.run(f"rm -r {obj_folder}/ll{number}", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     with open(stderr_temp, "w") as stderr_temp:
-        subprocess.run(f"cd {program_folder_path} && make && make llvm && mv ./ll/program.ll ./../../scripts/grammarEnumeration/obj", shell=True, check=True, stdout=subprocess.DEVNULL, stderr=stderr_temp)
+        subprocess.run(f"cd {program_folder_path} && make llvm && mv ./ll ./../../scripts/grammarEnumeration/obj/ll{number}", shell=True, check=True, stdout=subprocess.DEVNULL, stderr=stderr_temp)
 
-def doAnalysis(program_number, analysis_file, obj_folder):
-    stderr_temp = os.path.join(obj_folder, "stderr_temp.txt")
-    with open(stderr_temp, "r") as stderr_temp:
-        stderr = stderr_temp.read()
-        if stderr:
-            analysis_file.write(f"{program_number}: ERROR!\n")
-            return 0
-        else:
-            isEmptyFile = os.path.join(obj_folder, "isEmpty.txt")
-            with open(isEmptyFile, "w") as f:
-                subprocess.run(f"./emptinessChecker.sh obj/program.ll", shell=True, stdout=f, stderr=subprocess.DEVNULL, check=True)
-            with open(isEmptyFile, "r") as f:
-                content = f.read()
-                if content.strip() == "1":
-                    analysis_file.write(f"{program_number}: EMPTY!\n")
-                    return 1
-                else:
-                    analysis_file.write(f"{program_number}: OK!\n")
-                    return 2
+def doAnalysis(program_number, analysis_file, obj_folder, nums):
+    total_lines = {}
+    for num in nums:
+        stderr_temp = os.path.join(obj_folder, f"stderr_temp{num}.txt")
+        with open(stderr_temp, "r") as stderr_temp:
+            stderr = stderr_temp.read()
+            if stderr:
+                analysis_file.write(f"{program_number}: ERROR!\n")
+                return 0
+            else:
+                isEmptyFile = os.path.join(obj_folder, "isEmpty.txt")
+                with open(isEmptyFile, "w") as f:
+                    subprocess.run(f"./emptinessChecker.sh obj/ll{num}/program{num}.ll", shell=True, stdout=f, stderr=subprocess.DEVNULL, check=True)
+                with open(isEmptyFile, "r") as f:
+                    content = f.read()
+                    if content.strip() == "1":
+                        analysis_file.write(f"{program_number}: EMPTY!\n")
+                        return 1
+                    else:
+                        total_lines[num] = getTotalLines(obj_folder, num)
+    if total_lines[nums[0]] == total_lines[nums[1]]:
+        analysis_file.write(f"{program_number}: DOES NOT GROW!\n")
+        return 2
+    else:
+        analysis_file.write(f"{program_number}: SUCCESS!\n")
+        return 3
+
+def getTotalLines(obj_folder, number):
+    target_folder = os.path.join(obj_folder, f"ll{number}")
+    total_lines = 0
+    if not os.path.exists(target_folder):
+        raise FileNotFoundError(f"The folder '{target_folder}' does not exist.")
+    for file_name in os.listdir(target_folder):
+        if file_name.endswith(".ll"):
+            file_path = os.path.join(target_folder, file_name)
+            with open(file_path, 'r') as file:
+                total_lines += sum(1 for _ in file)
+    return total_lines
+    
 
 def analyzePrograms(path):
     file = os.path.join(path, "programs.txt")    
@@ -64,9 +86,11 @@ def analyzePrograms(path):
     os.makedirs(obj_folder, exist_ok=True)
     number_of_errors = 0
     number_of_successes = 0
+    number_of_does_not_grow = 0
     number_of_empty = 0
     with open(file, "r") as f:
         lineC = round((f.read().count('\n')) / 4)
+
     with open(file, "r") as f, open(analysis, "w") as analysis_file:
         current_program = {}
         for i, line in enumerate(f):
@@ -80,26 +104,35 @@ def analyzePrograms(path):
             elif line.startswith("B ="):
                 current_program['rule_b'] = line.split("=")[1].strip().strip(";")
                 createProgramFiles(obj_folder, current_program)
-                program_folder_path = generateProgram()
-                compileProgram(obj_folder, program_folder_path)
-                result = doAnalysis(current_program["number"], analysis_file, obj_folder)
+                n0 = 4
+                n1 = 6
+                program0_folder_path = generateProgram(n0)
+                program1_folder_path = generateProgram(n1)
+                compileProgram(obj_folder, program0_folder_path, n0)
+                compileProgram(obj_folder, program1_folder_path, n1)
+                result = doAnalysis(current_program["number"], analysis_file, obj_folder, [n0, n1])
                 if result == 0:
                     number_of_errors += 1
                 elif result == 1:
                     number_of_empty += 1
+                elif result == 2:
+                    number_of_does_not_grow += 1
                 else:
                     number_of_successes += 1
                 current_program = {}
                 print("Program ", round((i + 1)/4), "/", lineC)
+
     with open(analysis, "r") as file:
         original_content = file.readlines()
     with open(analysis, "w") as file:
-        total = number_of_successes + number_of_errors + number_of_empty
+        total = number_of_successes + number_of_errors + number_of_empty + number_of_does_not_grow
         p_success = round(number_of_successes / (total) * 100)
         p_errors = round(number_of_errors / (total) * 100)
         p_empty = round(number_of_empty / (total) * 100)
+        p_does_not_grow = round(number_of_does_not_grow / (total) * 100)
         file.write(f"Number of programs: {total}\n")
         file.write(f"% of successes: {p_success}%\n")
+        file.write(f"% of programs that do not grow: {p_does_not_grow}%\n")
         file.write(f"% of errors: {p_errors}%\n")
         file.write(f"% of empty programs: {p_empty}%\n")
         file.write("\n")
