81d2e56d8db4ed35051ca2ee61de4f1fb287a899
Vinicius Silva <viniciusilva@ieee.org>
2025-08-30 20:26:11 -0300

    adjust rand loopsfactor

--- src/gen/ast/cpp/cppAst.cpp
+++ src/gen/ast/cpp/cppAst.cpp
@@ -84,7 +84,7 @@ void CppLoop::gen(ProgrammingLanguageGenerator& generator) {
     generator.addLine(loopVarLine);
 
     std::string loopLimitVar = "loopLimit" + std::to_string(generator.loopCounter);
-    std::string loopLimitValue = "(1000000000%loopsFactor)/" + std::to_string(generator.loopLevel + 1) + " + 1";
+    std::string loopLimitValue = "(1000000000)/" + std::to_string(generator.loopLevel + 1) + " + 1";
     std::string loopLimitLine = "unsigned int " + loopLimitVar + " = " + loopLimitValue + ";";
     generator.addLine(loopLimitLine);
 
